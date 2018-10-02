e5e1cf31006731d1fad60c1858420127e46fb47c
0jordanmiller@gmail.com <0jordanmiller@gmail.com>
2018-10-02 19:32:53 -0400

    yo

--- client/src/components/utils/API.js
+++ client/src/components/utils/API.js
@@ -1,16 +1,25 @@
 import axios from "axios";
 
-const BASEURL = function (query) {
-  const key_arr = ["AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc", "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc", "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc", "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc"];
-  const key = key_arr[Math.floor(Math.random() * key_arr.length)]
+const BASEURL = function(query) {
+  const key_arr = [
+    "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc",
+    "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc",
+    "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc",
+    "AIzaSyDTzb-iUAYqARgI2ydk8pkO8UTuKSC8mjc"
+  ];
+  const key = key_arr[Math.floor(Math.random() * key_arr.length)];
   console.log("raleigh");
-  return 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + query + '&key=' + key
+  return (
+    "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" +
+    query +
+    "&key=" +
+    key
+  );
+};
 
-}
-    
 export default {
-  search: function (query) {
-    const url = BASEURL(query)
-    return axios.get(url)
+  search: function(query) {
+    const url = BASEURL(query);
+    return axios.get(url);
   }
-}; 
+};
